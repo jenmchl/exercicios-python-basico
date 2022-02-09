3bfbb51220848745283b6305b74ee3f74b624587
Jean Michel <jeanmichel@jeanmichel.home>
2022-02-09 23:50:06 +0100

    exercício par ou ímpar

---
+++ par-ou-impar.py
@@ -0,0 +1,12 @@
+
+
+numero1 = int(input('Qual é o valor do Jogador 1: '))
+
+numero2 = int(input('Qual é o valor do Jogador 2: '))
+
+resultado_soma = numero1 + numero2
+
+if resultado_soma %2 == 0:
+    print('Par')
+else:
+    print('Ímpar')
