25bfde1b826523219580ef17164fdbc623ba4886
Jean Michel <jeanmichel@jeanmichel.home>
2022-02-09 16:18:32 +0100

    exercicio while e for

---
+++ exercicio-aula5.py
@@ -0,0 +1,16 @@
+
+# Exercicio: Faça um programa que leia a quantidade de pessoas que
+# serao convidadas para uma festa.
+# Após isso o programa ira perguntar o nome de todas as pessoas e colocar numa
+# lista de convidados
+# Após isso ira imprimir todos os nomes da lista
+
+
+lista_convidados = ('Jean', 'Bárbara', 'Adriane', 'Gustavo', 'Ana', 'Giovane',
+                    'Samanta', 'Janete', 'Mayara', 'Murilo', 'Vitória', 'Angela', 'Márcio')
+
+print(len(lista_convidados),'pessoas estão convidadas para a festa!')
+
+print('Os convidados são:')
+for i in range(len(lista_convidados)):
+    print(lista_convidados[i])
